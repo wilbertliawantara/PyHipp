a2bd63b9aa209515acaae7b68e3db4c06d68ede6
AWS ParallelCluster user <ec2-user@ip-10-0-5-119.ap-southeast-1.compute.internal>
2025-09-06 11:43:07 +0000

    Lab 5

--- PyHipp/rplhighpass-sort-slurm.sh
+++ PyHipp/rplhighpass-sort-slurm.sh
@@ -20,7 +20,11 @@ conda activate $envarg
 
 python -u -c "import PyHipp as pyh; \
 import time; \
-pyh.RPLLFP(saveLevel=1); \
+pyh.RPLHighPass(saveLevel=1); \
+from PyHipp import mountain_batch; \
+mountain_batch.mountain_batch(); \
+from PyHipp import export_mountain_cells; \
+export_mountain_cells.export_mountain_cells(); \
 print(time.localtime());"
 
 conda deactivate 
